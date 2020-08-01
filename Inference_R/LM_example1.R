9a5cf1a3b20b20ffedb4eea5ba7568ca672a1705
Rishi Subedi <rishiram.subedi@gmail.com>
2020-08-01 20:32:51 +0545

    feat: Added R code to fit the data

--- Inference_R/LM_example1.R
+++ Inference_R/LM_example1.R
@@ -1,1 +1,11 @@
+#reading the files from the data
+aids<-read.csv("aids.csv")
+#using year as a response and deaths as explanatory variable
+fit.death<-lm(year~deaths,data=aids)
+#plot the scatterplot
+plot(year~deaths,data = aids)
+#plot the regression line in scatter plot
+abline(fit.death,col="red")
+summary(fit.death)
+
 
