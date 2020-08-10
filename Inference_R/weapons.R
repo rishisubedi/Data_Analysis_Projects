203922df5c19a66aa0d46da641fcf424d24f2637
Rishi Subedi <rishiram.subedi@gmail.com>
2020-08-10 22:26:42 +0545

    Analysis of Weapons.csv continued

--- Inference_R/weapons.R
+++ Inference_R/weapons.R
@@ -4,3 +4,12 @@ weapons <- read.csv("~/weapons.csv")
 summary(weapons)
 # counting the null values
 sum(is.na(weapons))
+#hypothesis testing using t.test using all variables
+t.test(aw~sex,data=weapons)
+t.test(an~sex,data=weapons)
+t.test(cw~sex,data=weapons)
+t.test(cn~sex,data=weapons)
+# Finding relationship between weapons "primer" and aggression word
+da<- weapons$aw-weapons$an
+# Finding relationship between weapons "primer" and neutral word
+dc<- weapons$cw-weapons$cn
