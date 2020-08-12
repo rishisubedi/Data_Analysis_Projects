369b280c13dbedf15428517c6200887d4231936e
Rishi Subedi <rishiram.subedi@gmail.com>
2020-08-12 21:31:29 +0545

    feat: Linear Regression Model Added

--- Inference_R/weapons.R
+++ Inference_R/weapons.R
@@ -13,3 +13,10 @@ t.test(cn~sex,data=weapons)
 da<- weapons$aw-weapons$an
 # Finding relationship between weapons "primer" and neutral word
 dc<- weapons$cw-weapons$cn
+#variance test
+var.test(aw~sex,data=weapons)
+var.test(an~sex,data=weapons)
+var.test(cn~sex,data=weapons)
+var.test(cw~sex,data=weapons)
+#Applying linear regression model
+lm.weapon<-lm(aw~an, data=weapons)
